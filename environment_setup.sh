87afb3ba96fb8fa827e1ffd5943d9cdc6db80b1c
Shuo Wang <72591695+markinruc@users.noreply.github.com>
2025-07-04 17:15:56 +0800

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-CONDA_ENV=${1:-""}
-if [ -n "$CONDA_ENV" ]; then
-    # This is required to activate conda environment
-    eval "$(conda shell.bash hook)"
-
-    conda create -n $CONDA_ENV python=3.10.14 -y
-    conda activate $CONDA_ENV
-    # This is optional if you prefer to use built-in nvcc
-    conda install -c nvidia cuda-toolkit -y
-else
-    echo "Skipping conda environment creation. Make sure you have the correct environment activated."
-fi
-
 # This is required to enable PEP 660 support
 pip install --upgrade pip setuptools
 
